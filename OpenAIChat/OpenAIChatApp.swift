a3a81739816757b159fb4856574f0af113473c30
Marcin Wiatr <marcinwiatr@hotmail.com>
2024-07-02 09:30:31 -0700

    Initial Commit

---
+++ OpenAIChat/OpenAIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIChatApp.swift
+//  OpenAIChat
+//
+//  Created by MarcinWiatr on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
